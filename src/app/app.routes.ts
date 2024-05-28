6805b6f6e249dd960ba3f0ac9005b82e117f578f
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-05-27 22:02:28 -0300

    feat: Add button component and landing page layout
    - Added a new button component with HTML, CSS, and TypeScript files.
    - Created a landing page layout with a header, navigation, and section content.
    - Updated the app routes to include the landing page component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './shared/views/landing-page/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    }
+];
--- src/app/shared/views/landing-page/landing-page/landing-page.component.ts
+++ src/app/shared/views/landing-page/landing-page/landing-page.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../components/button/button.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
 export class LandingPageComponent {
 
+  constructor() { }
+
+  title = 'Landing Page';
 }
