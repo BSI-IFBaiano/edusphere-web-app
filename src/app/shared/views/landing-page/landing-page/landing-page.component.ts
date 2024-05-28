e31779bc7572d4c05ff0b77f382aef498b5a7546
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-05-27 23:16:01 -0300

    feat: Add SVG file for ellipse shape and update simple card component
    - Added a new SVG file for the ellipse shape to be used in the simple card component.
    - Updated the simple card component to display the ellipse shape as a default image if no image is provided.
    - Added the necessary imports and dependencies for the simple card component.

--- src/app/shared/views/landing-page/landing-page/landing-page.component.ts
+++ src/app/shared/views/landing-page/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
+import { SimpleCardComponent } from '../../../components/simple-card/simple-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, SimpleCardComponent, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
