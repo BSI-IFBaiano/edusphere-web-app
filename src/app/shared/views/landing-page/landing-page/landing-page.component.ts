839e68367625c94f74501d9ba18ee0e4b3ac8a49
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-05-28 06:42:10 -0300

    feat: Add course card component and update landing page
    - Added a new course card component with HTML, CSS, and TypeScript files.
    - Updated the landing page to include the course card component and display multiple course cards.
    - Imported the necessary dependencies and modules for the course card component and landing page.

--- src/app/shared/views/landing-page/landing-page/landing-page.component.ts
+++ src/app/shared/views/landing-page/landing-page/landing-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../../../components/button/button.component';
 import { SimpleCardComponent } from '../../../components/simple-card/simple-card.component';
+import { CourseCardComponent } from '../../../components/course-card/course-card.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ButtonComponent, SimpleCardComponent, CommonModule],
+  imports: [ButtonComponent, SimpleCardComponent, CommonModule, CourseCardComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
