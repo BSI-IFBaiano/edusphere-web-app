82a32520d13661fc0a7ee8e661adb0d579db0d7e
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-06-04 07:43:09 -0300

    refactor: Remove unused hiddenRoutes property from environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3003/api/',
-  hiddenRoutes : ['/login', '/register', '/forgot-password', '/new-password', ''],
 };
 
 //https://api-email-services-dev.onrender.com/api/
