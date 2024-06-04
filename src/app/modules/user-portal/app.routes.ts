d2e5fb454b33a0da982fa8b1b39d21a18c63daae
Wesley Ulisses <wesleyulisses0@gmail.com>
2024-06-04 07:29:40 -0300

    feat: Update login component with forget password link
    The login component HTML file was modified to update the forget password link. It now navigates to the "forget-password" route instead of the home route.

--- src/app/modules/user-portal/app.routes.ts
+++ src/app/modules/user-portal/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../../shared/views/login/login.component';
 import { LandingPageComponent } from '../../shared/views/landing-page/landing-page/landing-page.component';
-
+import { ForgetPasswordComponent } from '../../shared/views/forget-password/forget-password.component';
 
 export const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
-  { path: '', component: LandingPageComponent }
- 
+  { path: '', component: LandingPageComponent },
+  { path: 'forget-password', component: ForgetPasswordComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/shared/services/forget-password/forget-password.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { ForgetPasswordService } from './forget-password.service';
+
+describe('ForgetPasswordService', () => {
+  let service: ForgetPasswordService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ForgetPasswordService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
